468c1907837b2a34097522bac3e6b4754f043547
Abrar <ahmedabrararian@gmail.com>
2024-12-22 22:27:39 +0600

    Updated banner with a bouncing image

--- src/pages/Home/HomeComponents/Banner.jsx
+++ src/pages/Home/HomeComponents/Banner.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import SliderForBanner from "./SliderForBanner";
+import { motion } from "motion/react"
+import banner1 from "../../../assets/banner-1.webp"
 
 const Banner = () => {
   return (
-    <div className="bg-[#f9f8fd]">
-      <div className="max-w-screen-2xl mx-auto px-4 py-10 flex flex-col-reverse md:flex-row gap-4 items-center justify-between">
+    <div className="bg-[#f9f8fd] py-8">
+      <div className="max-w-screen-2xl mx-auto px-4 py-10 flex flex-col-reverse md:flex-row gap-8 items-center justify-between">
         <div className="xxxs:max-w-80 xxs:max-w-96 xs:max-w-md sm:max-w-2xl md:max-w-96 mdb:max-w-md lg:max-w-xl lgx:max-w-2xl">
           {/* Slider & Buttons on this left side */}
           {/* <p>Left Side</p> */}
@@ -12,7 +13,10 @@ const Banner = () => {
         </div>
         <div>
           {/* Image and design on this right side */}
-          <p>Right Side</p>
+          <div>
+            <motion.img src={banner1} alt=""
+            animate={{ y: ["3%", "-6%", "3%"] }} transition={{ duration: 3, repeat: Infinity, repeatType: "mirror", ease: "easeInOut" }} />
+          </div>
         </div>
       </div>
     </div>
