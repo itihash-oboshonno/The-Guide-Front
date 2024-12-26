d46f299a7fda55590a6147c5b75371ed5e1adcfb
Abrar <ahmedabrararian@gmail.com>
2024-12-26 12:09:04 +0600

    Added our process section at homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import Banner from './HomeComponents/Banner';
 import RecentBlogs from './HomeComponents/RecentBlogs';
 import SubNewsLetter from './HomeComponents/SubNewsLetter';
 import FAQ from './HomeComponents/FAQ';
+import BannerTwo from './HomeComponents/BannerTwo';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <RecentBlogs></RecentBlogs>
+            <BannerTwo></BannerTwo>
             <FAQ></FAQ>
             <SubNewsLetter></SubNewsLetter>
         </div>
