f733b9bb01db996a9ed4917809d9217688175a9e
Abrar <ahmedabrararian@gmail.com>
2024-12-26 10:56:27 +0600

    Updated ErrorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import errorImg from '../../assets/404-error.png'
 
 const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className='flex flex-col items-center justify-center gap-10'>
+            <img className='max-h-52 mx-auto p-5' src={errorImg} alt="" />
+            <p className='text-center font-bold'>Something went wrong... Page not found!</p>
+            <Link to='/'><button className="text-dark font-bold px-6 py-3 rounded-full bg-prim3 cursor-pointer hover:shadow-lg">Go to Homepage</button></Link>
         </div>
     );
 };
